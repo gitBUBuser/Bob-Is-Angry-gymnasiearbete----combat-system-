941b645bdbd39be11e2555f7d65685ee2728cca7
ProGamerGRILL 34.5 <32479781+Gamemaste2@users.noreply.github.com>
2019-09-12 15:31:22 +0200

    randy random

---
+++ GymnasieArbete (tempName)/Assets/Scripts/BoundaryManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryManager : MonoBehaviour
+{
+    BoxCollider2D managerBox;
+    Transform player;
+    GameObject boundary;
+
+    private void Start()
+    {
+        managerBox = GetComponent<BoxCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void ManageBoundary()
+    {
+
+    }
+}
